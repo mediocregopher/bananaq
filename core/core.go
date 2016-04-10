657eb31693503b7185afc6f175d24d90431e93f5
Brian Picciano <mediocregopher@gmail.com>
2016-04-10 12:34:02 -0600

    fix NewID, implement Get/SetEvent

--- core/core.go
+++ core/core.go
@@ -3,18 +3,27 @@
 package core
 
 import (
+	"errors"
+	"fmt"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/levenlabs/go-llog"
 	"github.com/levenlabs/golib/radixutil"
 	"github.com/levenlabs/golib/timeutil"
+	"github.com/mediocregopher/radix.v2/redis"
 	"github.com/mediocregopher/radix.v2/util"
 )
 
 var (
 	c     util.Cmder
 	pkgKV = llog.KV{"pkg": "core"}
+)
+
+// Various errors this package may return
+var (
+	ErrNotFound = errors.New("not found")
 )
 
 // Init initializes the core global variables and gets the party started
@@ -33,51 +42,122 @@ func Init(redisAddr string, poolSize int) {
 const idKey = "id"
 
 // ID identifies a single event across the entire cluster, and is unique for all
-// time. It's also a timestamp which describes when the event is no longer
-// valid.
-type ID float64
-
-// Time returns a time object which corresponds with the id/timestamp
-func (id ID) Time() time.Time {
-	return timeutil.TimestampFromFloat64(float64(id)).Time
-}
+// time
+type ID int64
 
 // NewID returns a new, unique ID which can be used for a new event
 func NewID() (ID, error) {
-	return newID(timeutil.TimestampNow().Float64())
+	return newID(newIDBase(time.Now()))
+}
+
+func newIDBase(now time.Time) int64 {
+	// We unfortunately are stuck using microseconds because lua gets kind of
+	// weird at bigger numbers in converting to/from strings
+	return now.UnixNano() / 1e3
 }
 
 // We split this out to make testing easier
-func newID(now float64) (ID, error) {
+func newID(now int64) (ID, error) {
 	lua := `
 		local key = KEYS[1]
 		local nowStr = ARGV[1]
 		local lastStr = redis.call("GET", key)
-		
-		if not lastStr then
+
+		local now  = tonumber(nowStr, 10)
+		local last = tonumber(lastStr, 10)
+
+		if not lastStr or not last then
 			redis.call("SET", key, nowStr)
 			return nowStr
 		end
 
-		local now  = tonumber(nowStr, 10)
-		local last = tonumber(lastStr, 10)
-		if not now or not last or last < now then
+		if last < now then
 			redis.call("SET", key, nowStr)
 			return nowStr
 		end
 
 		-- Add a microsecond and use that
-		last = last + 0.000001
-		lastStr = tostring(last)
+		last = last + 1
+		lastStr = string.format("%.0f", last)
 		redis.call("SET", key, lastStr)
 		return lastStr
 	`
 
-	fStr, err := util.LuaEval(c, lua, 1, idKey, now).Str()
+	iStr, err := util.LuaEval(c, lua, 1, idKey, now).Str()
 	if err != nil {
 		return 0, err
 	}
 
-	f, err := strconv.ParseFloat(fStr, 64)
-	return ID(f), err
+	i, err := strconv.ParseInt(iStr, 10, 64)
+	return ID(i), err
+}
+
+// Time returns a time object which corresponds with when the id was created
+func (id ID) Time() time.Time {
+	return time.Unix(0, int64(id)*1e3)
+}
+
+// Timeout identifies the amount of time something is valid for before it will
+// be cleaned from whatever context it exists in. It is represented as number of
+// seconds, with everything passed the decimal point representing sub-second
+// precision
+type Timeout float64
+
+// Abs is like AbsTime, but returns the time as a unix timestamp, with
+// everything after the decimal representing sub-second precision
+func (t Timeout) Abs(starting time.Time) float64 {
+	return timeutil.Timestamp{starting}.Float64() + float64(t)
+}
+
+// AbsTime returns the point in time when this timeout is over, assuming it
+// starts at the given time.
+func (t Timeout) AbsTime(starting time.Time) time.Time {
+	return timeutil.TimestampFromFloat64(t.Abs(starting)).Time
+}
+
+func eventKey(id ID) string {
+	return fmt.Sprintf("event:%f", id)
+}
+
+func queueKey(queueName string, parts ...string) string {
+	return fmt.Sprintf("queue:{%s}:%s", queueName, strings.Join(parts, ":"))
+}
+
+func groupKey(queueName, groupName string, parts ...string) string {
+	fullParts := make([]string, 0, len(parts)+1)
+	fullParts = append(fullParts, groupName)
+	fullParts = append(fullParts, parts...)
+	return queueKey(queueName, fullParts...)
+}
+
+func extractQueueName(key string) string {
+	i, j := strings.Index(key, "{"), strings.Index(key, "}")
+	return key[i+1 : j]
+}
+
+func extractGroupName(key string) string {
+	i := strings.Index(key, "}")
+	key = key[i+2:]
+	j := strings.Index(key, ":")
+	if j == -1 {
+		return key
+	}
+	return key[:j]
+}
+
+// SetEvent sets the event with the given id to have the given contents. The
+// event will expire after the given timeout (which will be truncated to an
+// integer)
+func SetEvent(id ID, contents string, timeout Timeout) error {
+	return c.Cmd("SET", eventKey(id), contents, "EX", int64(timeout)).Err
+}
+
+// GetEvent returns the event identified by the given id, or ErrNotFound if it's
+// expired or never existed
+func GetEvent(id ID) (string, error) {
+	r := c.Cmd("GET", eventKey(id))
+	if r.IsType(redis.Nil) {
+		return "", ErrNotFound
+	}
+	return r.Str()
 }
--- core/core_test.go
+++ core/core_test.go
@@ -2,9 +2,11 @@ package core
 
 import (
 	. "testing"
+	"time"
 
-	"github.com/levenlabs/golib/timeutil"
+	"github.com/levenlabs/golib/testutil"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func init() {
@@ -12,17 +14,61 @@ func init() {
 }
 
 func TestNewID(t *T) {
-	now := timeutil.TimestampNow().Float64()
-	id, err := newID(now)
-	assert.Nil(t, err)
-	assert.Equal(t, ID(now), id)
+	for i := 0; i < 100; i++ {
+		now := time.Now()
+		nowI := newIDBase(now)
+		id, err := newID(nowI)
+		assert.Nil(t, err)
+		assert.Equal(t, ID(nowI), id)
 
-	id2, err := newID(now)
-	assert.Nil(t, err)
-	assert.True(t, id2 > id)
+		// make sure at least the second precision matches, the actual times
+		// won't match exactly because we truncate to microseconds
+		assert.Equal(t, now.Unix(), id.Time().Unix())
 
-	now = timeutil.TimestampNow().Float64()
-	id3, err := newID(now)
+		id2, err := newID(nowI)
+		assert.Nil(t, err)
+		assert.True(t, id2 > id, "id2:%s !> id:%s ", id2, id)
+
+		nowI = newIDBase(time.Now())
+		id3, err := newID(nowI)
+		assert.Nil(t, err)
+		assert.True(t, id3 > id2, "id3:%s !> id2:%s ", id3, id2)
+	}
+}
+
+func TestKeys(t *T) {
+	queueName := testutil.RandStr()
+	groupName := testutil.RandStr()
+
+	key := queueKey(queueName)
+	assert.Equal(t, queueName, extractQueueName(key))
+	key = queueKey(queueName, "foo")
+	assert.Equal(t, queueName, extractQueueName(key))
+
+	key = groupKey(queueName, groupName)
+	assert.Equal(t, queueName, extractQueueName(key))
+	assert.Equal(t, groupName, extractGroupName(key))
+	key = groupKey(queueName, groupName, "foo")
+	assert.Equal(t, queueName, extractQueueName(key))
+	assert.Equal(t, groupName, extractGroupName(key))
+}
+
+func requireNewID(t *T) ID {
+	id, err := NewID()
+	require.Nil(t, err)
+	return id
+}
+
+func TestGetSetEvent(t *T) {
+	id := requireNewID(t)
+	contents := testutil.RandStr()
+
+	assert.Nil(t, SetEvent(id, contents, 1))
+	contents2, err := GetEvent(id)
 	assert.Nil(t, err)
-	assert.True(t, id3 > id2)
+	assert.Equal(t, contents, contents2)
+
+	time.Sleep(1*time.Second + 100*time.Millisecond)
+	_, err = GetEvent(id)
+	assert.Equal(t, ErrNotFound, err)
 }
