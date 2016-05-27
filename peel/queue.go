f8cadb0134756e44f6ed762e247f4d042957ed1a
Brian Picciano <mediocregopher@gmail.com>
2016-05-26 21:57:54 -0600

    clean up data being passed from peel to core

--- peel/queue.go
+++ peel/queue.go
@@ -1,19 +1,26 @@
 package peel
 
-import "github.com/mediocregopher/bananaq/core"
+import (
+	"encoding/hex"
 
-// TODO need to make sure everything all fields here are alphanumeric, or maybe
-// change the separator in redis to be \0 or something
+	"github.com/mediocregopher/bananaq/core"
+)
+
+func queueNameClean(queue, cgroup string) (string, string) {
+	return hex.EncodeToString([]byte(queue)), hex.EncodeToString([]byte(cgroup))
+}
 
 // Keeps track of events which are available to be retrieved by any particular
 // consumer group, with scores corresponding to the event's id.
 func queueAvailableByID(queue string) core.Key {
+	queue, _ = queueNameClean(queue, "")
 	return core.Key{Base: queue, Subs: []string{"available", "id"}}
 }
 
 // Keeps track of events which are available to be retrieved by any particular
 // consumer group, with scores corresponding to the event's expire time.
 func queueAvailableByExpire(queue string) core.Key {
+	queue, _ = queueNameClean(queue, "")
 	return core.Key{Base: queue, Subs: []string{"available", "expire"}}
 }
 
@@ -21,6 +28,7 @@ func queueAvailableByExpire(queue string) core.Key {
 // corresponding to the event's id. Used to retrieve the next available event in
 // a queue
 func queueInProgressByID(queue, cgroup string) core.Key {
+	queue, cgroup = queueNameClean(queue, cgroup)
 	return core.Key{Base: queue, Subs: []string{cgroup, "inprogress", "id"}}
 }
 
@@ -28,23 +36,27 @@ func queueInProgressByID(queue, cgroup string) core.Key {
 // corresponding to the event's ack deadline. Used to timeout in progress events
 // and put them in redo
 func queueInProgressByAck(queue, cgroup string) core.Key {
+	queue, cgroup = queueNameClean(queue, cgroup)
 	return core.Key{Base: queue, Subs: []string{cgroup, "inprogress", "ack"}}
 }
 
 // Keeps track of events which were previously attempted to be processed but
 // failed. Score is the event's id
 func queueRedo(queue, cgroup string) core.Key {
+	queue, cgroup = queueNameClean(queue, cgroup)
 	return core.Key{Base: queue, Subs: []string{cgroup, "redo"}}
 }
 
 // Keeps track of events which have been successfully processed. Score is the
 // event's id
 func queueDone(queue, cgroup string) core.Key {
+	queue, cgroup = queueNameClean(queue, cgroup)
 	return core.Key{Base: queue, Subs: []string{cgroup, "done"}}
 }
 
 // Keeps track of all events currently in use by a cgroup which haven't expired.
 // Used in order to clean expired events out of a group
 func queueInUseByExpire(queue, cgroup string) core.Key {
+	queue, cgroup = queueNameClean(queue, cgroup)
 	return core.Key{Base: queue, Subs: []string{cgroup, "inuse"}}
 }
