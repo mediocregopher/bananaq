50c4f81c5983cb0f397aee05437f461d1440c95e
Brian Picciano <mediocregopher@gmail.com>
2016-05-28 14:44:01 -0600

    turn hex encoding of keys back on

--- peel/queue.go
+++ peel/queue.go
@@ -1,30 +1,33 @@
 package peel
 
-import "github.com/mediocregopher/bananaq/core"
+import (
+	"encoding/hex"
 
-// TODO hex encoding everything super sucks, and makes things hard to debug
+	"github.com/mediocregopher/bananaq/core"
+)
+
+// TODO this hex encoding thing makes debugging a lot harder, would be nice to
+// not have to do it
 
 func queueKeyMarshal(k core.Key) core.Key {
-	return k
-	//km := core.Key{
-	//	Base: hex.EncodeToString([]byte(k.Base)),
-	//	Subs: make([]string, len(k.Subs)),
-	//}
-	//for i, sub := range k.Subs {
-	//	km.Subs[i] = hex.EncodeToString([]byte(sub))
-	//}
-	//return km
+	km := core.Key{
+		Base: hex.EncodeToString([]byte(k.Base)),
+		Subs: make([]string, len(k.Subs)),
+	}
+	for i, sub := range k.Subs {
+		km.Subs[i] = hex.EncodeToString([]byte(sub))
+	}
+	return km
 }
 
 func queueKeyUnmarshal(k core.Key) core.Key {
-	return k
-	//b, _ := hex.DecodeString(k.Base)
-	//subs := make([]string, len(k.Subs))
-	//for i, sub := range k.Subs {
-	//	subum, _ := hex.DecodeString(sub)
-	//	subs[i] = string(subum)
-	//}
-	//return core.Key{Base: string(b), Subs: subs}
+	b, _ := hex.DecodeString(k.Base)
+	subs := make([]string, len(k.Subs))
+	for i, sub := range k.Subs {
+		subum, _ := hex.DecodeString(sub)
+		subs[i] = string(subum)
+	}
+	return core.Key{Base: string(b), Subs: subs}
 }
 
 // Keeps track of events which are available to be retrieved by any particular
